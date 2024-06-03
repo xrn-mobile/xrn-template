9badbfed08949b263eee36517f31c2c1bae840b2
Hao-yiwen <1552975589@qq.com>
2024-06-04 00:37:08 +0800

    xrn: update template

--- App.tsx
+++ App.tsx
@@ -1,41 +1,10 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from './src/Home';
-import WebViewPage from './src/WebViewPage';
-import {StatusBar} from 'react-native';
+import {Text, View} from 'react-native';
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <>
-      <StatusBar backgroundColor="#f4511e" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: 'Overview',
-              headerStyle: {
-                backgroundColor: '#f4511e',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-            }}
-          />
-          <Stack.Screen
-            name="WebViewPage"
-            component={WebViewPage}
-            options={{title: 'WebViewPage'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello, world!</Text>
+    </View>
   );
 }
-
-export default App;
